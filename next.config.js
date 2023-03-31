a0e9ced862023a790d6146155aa2181240b223f4
bold_jatin_sachdev <jatin.sachdev@bold.com>
2023-03-31 13:52:28 +0530

    d

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const originHeader = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*'
+  }
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/hello',
+        headers: originHeader,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method == "POST") {
+    var request = initializeRequest(req.body.prompt);
+    request
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')        
+        res.status(200).json({ name: data });
+      });
+  }
+}
+
+function initializeRequest(prompt) {
+  const proto = {
+    prompt: prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 1000,
+    stop: "string",
+    best_of: 1,
+  };
+
+  return fetch(
+    "https://api-gpt-playground.bold.com/gpt3/OpenAI_Text-DV-003/completions",
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(proto),
+      method: "POST",
+    }
+  );
 }

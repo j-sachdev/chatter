bb5efbeae84e04d44c517b260c63e58d1380fd1c
j-sachdev <129490855+j-sachdev@users.noreply.github.com>
2023-03-31 13:59:55 +0530

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -6,7 +6,8 @@ export default function handler(req, res) {
         return response.json();
       })
       .then((data) => {
-        res.setHeader('Access-Control-Allow-Origin', '*').status(200).json({ name: data });
+        res.setHeader('Access-Control-Allow-Origin', '*')        
+        res.status(200).json({ name: data });
       });
   }
 }
